5d6755ba118397c137778cada26bd83fb2d0f687
chic-page-6502 <zacharytq@gmail.com>
2019-08-13 01:01:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,14 @@
 class Song
+  @@genres = []
+  @@songs = []
+  @@artists = []
+  
   def initialize(song, artist, genre)
-
+    @song = song
+    @@songs << song
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genre << genre
   end
 end
